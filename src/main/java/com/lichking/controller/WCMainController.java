e0a793ac28ca3c4a09fcbcdcc706f990b5e2d518
boblandry <landry001@vip.qq.com>
2016-03-29 15:37:05 +0800

    公众号返回收到的消息
    Change-Id: I4eac2709afbb8351bb00671c396d1e68ce5cd405
    Signed-off-by: boblandry <landry001@vip.qq.com>

--- src/main/java/com/lichking/controller/WCMainController.java
+++ src/main/java/com/lichking/controller/WCMainController.java
@@ -2,11 +2,13 @@ package com.lichking.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +21,7 @@ import com.lichking.util.wechat.MessageUtil;
 @Controller
 public class WCMainController {
 	
+	Logger logger = Logger.getLogger(WCMainController.class);
 	
 	//微信服务器配置验证token入口
 	@RequestMapping(value="/WeChatPortal",method=RequestMethod.GET)
@@ -47,9 +50,9 @@ public class WCMainController {
 	}
 	
 	
-	@SuppressWarnings("unused")
 	@RequestMapping(value="/WeChatPortal",method=RequestMethod.POST)
 	public void vWeChatPortal_p(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		logger.info(new Date());
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
 		PrintWriter pw = resp.getWriter();
@@ -60,12 +63,13 @@ public class WCMainController {
 			String toUserName = map.get("ToUserName");
 			String msgType = map.get("MsgType");
 			String content = map.get("Content");
-			
+			//logger.info(msgType);
 			String message = null;
 			if(MessageUtil.MESSAGE_TEXT.equals(msgType)){
 				String response_content = content;
 				message = MessageUtil.initText(toUserName, fromUserName, response_content);
 			}
+			pw.print(message);
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
